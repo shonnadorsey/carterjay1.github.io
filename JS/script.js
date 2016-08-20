483d9de1188628bde27f7af3b8bf3e6b469ce6f0
carterjay1 <carterjay1@cox.net>
2016-08-20 11:46:22 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('button').click(function(){
-      $('img').fadeOut('slow');
+  $('.button').click(function(){
+      $('img').fadeOut('1000');
   });
   $('#button-2').click(function(){
-      $('img').faeIn('slow');
+      $('img').faeIn('1000');
   });
   
 });
