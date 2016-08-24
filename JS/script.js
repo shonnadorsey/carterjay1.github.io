47af8dd2e278bb6ba591cb3a99fce28aa1edac44
carterjay1 <carterjay1@cox.net>
2016-08-23 19:59:44 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   $('button').click(funtion(){
     $('img').toggle();
   }); 
-  $('h1').fadeIn(1000);
-  $('p').fadeIn(2000);
+  $('h1').toggle();
+  $('p').toggle();
   $('header').click(function(){
   	$(this).css(“background-color”, “black”);
   });
