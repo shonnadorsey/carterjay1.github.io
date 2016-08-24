f971c8d3dd3954622176440dacf782fd8d71880a
carterjay1 <carterjay1@cox.net>
2016-08-23 20:04:59 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  $('button').click(funtion(){
-    $('img').toggle();
+  $("button").click(funtion(){
+    $("img").toggle();
   }); 
-  $('h1').toggle();
-  $('p').toggle();
-  $('header').click(function(){
+  $("h1").toggle();
+  $("p").toggle();
+  $("header").click(function(){
   	$(this).css(“background-color”, “black”);
   });
-  $('#fancy-text').click(function(){
+  $("#fancy-text").click(function(){
     $(this).css({"font-style", "italic", "color":"purple", "font-size":"30px"});
  });
